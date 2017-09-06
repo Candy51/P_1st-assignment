3eb6ae0822348b5da01220d05d9067de13d98b09
Candy51 <31677753+Candy51@users.noreply.github.com>
2017-09-06 11:05:33 +0900

    My first homework of professor Jeong Tae Eui.

---
+++ homework.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(void)
+{
+   printf("1. Student Id Number:2017301051\n");
+   printf("2. Name:Lee Kang Eun(¿Ã∞≠¿∫)\n");
+   printf("3. Mobile Phone Number:010-8636-3039\n");
+   printf("4. My Github address: https://github.com/Candy51");
+   return 0;
+}
