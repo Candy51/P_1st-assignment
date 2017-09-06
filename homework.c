e32fbe6e62b1d37226a3a7463c9fab5609592616
Candy51 <31677753+Candy51@users.noreply.github.com>
2017-09-06 11:00:09 +0900

    Delete homework.c

--- homework.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(void)
-{
-   printf("1. Student Id Number:2017301051\n");
-   printf("2. Name:Lee Kang Eun(¿Ã∞≠¿∫)\n");
-   printf("3. Mobile Phone Number:010-8636-3039\n");
-   printf("4. My Github address: Candy51\n");
-   return 0;
-}
